ff0a312a918df89867f59476334aab3540d794dd
fdstar <sky_flying_1@sina.com>
2021-11-19 15:32:41 +0800

    增加账号密码校验错误次数限制Demo

--- src/CheckCodeHelper.Samples/ComplexHelperTest.cs
+++ src/CheckCodeHelper.Samples/ComplexHelperTest.cs
@@ -56,9 +56,24 @@ namespace CheckCodeHelper.Samples
             //ComplexHelper依赖ICodeSender.Key来获取实际的验证码发送者，如果找不到，会产生异常
             var complexHelper = serviceProvider.GetRequiredService<ComplexHelper>();
 
+            var bizFlag = configuration.GetValue<string>("BizFlag");
+            if (bizFlag == "LoginValidError")
+            {
+                LoginError(complexHelper);
+                return;
+            }
+            else
+            {
+                CodeValidator(complexHelper);
+            }
+        }
+
+        private static void CodeValidator(ComplexHelper complexHelper)
+        {
+            Console.WriteLine("****** 您当前正在进行验证码校验Demo ******");
+            var bizFlag = configuration.GetValue<string>("BizFlag");
             var senderKey = configuration.GetValue<string>("CurrentSenderKey");
             var receiver = configuration.GetValue<string>("Receiver");
-            var bizFlag = configuration.GetValue<string>("BizFlag");
             var code = CodeHelper.GetRandomNumber(); //生成随机的验证码
 
             Action getTimeAction = () =>
@@ -97,6 +112,53 @@ namespace CheckCodeHelper.Samples
                         break;
                     }
                 }
+            }
+        }
+
+        private static void LoginError(ComplexHelper complexHelper)
+        {
+            Console.WriteLine("****** 您当前正在进行密码校验错误Demo ******");
+            var senderKey = NoneSender.DefaultKey; //密码校验错误无需实际发送内容
+            var bizFlag = configuration.GetValue<string>("BizFlag");
+            var correct = "1"; //密码正确时，得到的code
+            var error = "0"; //密码错误时，得到的code
+            var correctPwd = "123456";//这里不管输入的是什么账号，密码假设为同一个密码
+            Console.WriteLine("所有账户的正确密码为：" + correctPwd);
+            do
+            {
+                var account = ReadLine("** 请输入您的账号 **", "账号不能为空");
+                var pwd = ReadLine("** 请输入您的密码 **", "密码不能为空");
+                //检验前进行一次发送，因为周期设置成只能发送一次，所以无需关心发送结果
+                var sendResult = complexHelper.SendCodeAsync(senderKey, account, bizFlag, correct).Result;
+                Console.WriteLine("周期设置结果:" + sendResult + " 注意周期设置结果不影响校验");
+
+                var code = GetCode(pwd);
+                var verifyResult = complexHelper.VerifyCodeAsync(senderKey, account, bizFlag, code, true).Result;//密码校验正确时，重置允许密码连续错误的次数
+                Console.WriteLine("您账号 {0} 的密码验证结果 {1}", account, verifyResult);
+            }
+            while (true);
+
+            string GetCode(string pwd)
+            {
+                return pwd == correctPwd ? correct : error;
+            }
+
+            string ReadLine(string title, string errorNotice)
+            {
+                Console.WriteLine(title);
+                do
+                {
+                    var input = Console.ReadLine();
+                    if (string.IsNullOrWhiteSpace(input))
+                    {
+                        Console.WriteLine(errorNotice);
+                    }
+                    else
+                    {
+                        return input;
+                    }
+                }
+                while (true);
             }
         }
     }
